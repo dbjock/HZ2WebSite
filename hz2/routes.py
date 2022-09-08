4a9175a4a8b3652fb4d49996b4daed7700f4d388
dbjock (john) <junkyardjojo@hotmail.com>
2022-09-08 17:46:13 -0500

    Logging enhancement and column fix for weapons

--- hz2/routes.py
+++ hz2/routes.py
@@ -15,7 +15,6 @@ extFMT = logging.Formatter(
 # Log Handlers
 console = logging.StreamHandler(sys.stdout)
 console.setLevel(logging.INFO)
-console.setLevel(logging.DEBUG)
 console.setFormatter(extFMT)
 log = logging.getLogger(__name__)
 log.setLevel(logging.DEBUG)
@@ -37,57 +36,85 @@ def resources():
 @app.route("/weapon/<weapon_id>")
 def weapon_detail(weapon_id):
     weapon = Weapon.query.filter_by(id=weapon_id).first()
-    log.info(f"Get weapon result: {weapon}")
+    log.info(f"Weapon id : {weapon_id} results: {weapon}")
     if weapon == None:
+        log.info(f"Weapon was not found. Loading weapon not found page")
         return render_template('not_found.html',title='Weapon not found', thing='weapon')
 
     # Get the weapon requirements
     w_reqs = Weapon_requirement.query.filter_by(weapon_id=weapon.id).all()
     if len(w_reqs) == 0:
         return render_template('weapon.html', title=f"Weapon - {weapon.title}", weapon=weapon, headings=None)
+    log.debug(f"weapon requirement results= {w_reqs}")
 
+    log.info(f"Building list for panda")
     x_tmp_list=[]
+    # Require at least 5 columns
+    for x in range(1,6):
+        x_tmp_list.append(['skip','Force minimum number of pivot columns',x,0])
+    # Load weapon requirements
     for req in w_reqs:
         x_tmp_list.append([req.resource_id,req.resource.title,req.level,req.amt_required])
 
-    # Creating panda dataframe from weapon requirements
     log.debug(f"x_tmp_list:\n {x_tmp_list}")
+    # Creating panda dataframe from weapon requirements
+    log.info("Loading list into panda dataframe")
     x_tmp_df = pd.DataFrame(x_tmp_list, columns= ['resource_id','resource','level','amt_req'])
+    log.debug(f"dataframe: {x_tmp_df}")
+
     # Pivot for totals colum and row
+    log.info('Creating pivot table from dataframe')
     x_tmp_pivotTable = x_tmp_df.pivot_table(index=['resource','resource_id'],columns='level',values='amt_req', aggfunc=['sum'], margins=True, margins_name='Total').fillna(0).astype(int)
+    log.debug(f"pivottable: {x_tmp_pivotTable}")
 
     # Transform pivot table with totals to a list - Step 1: Convert to csv string
-    # x_tmp_pivot_string = x_tmp_pivotTable.to_csv(quoting=csv.QUOTE_NONNUMERIC,float_format=None)
+    log.info('Transform pivot data: Step 1 - pivot table -> csv string')
     x_tmp_pivot_string = x_tmp_pivotTable.to_csv(quoting=csv.QUOTE_NONNUMERIC,float_format=None)
+    del x_tmp_pivotTable
+    log.debug(f"pivottable -> string: {x_tmp_pivot_string}")
+
     # Transform pivot table with totals to a list - Step 2: Convert csv string to list
+    log.info('Transform pivot data: Step 2 - csv string -> list')
     x_tmp_pivot_list = list(csv.reader(x_tmp_pivot_string.split('\n')))
+    del x_tmp_pivot_string
+    log.debug(f"string -> list: {x_tmp_pivot_list}")
+
     # Transform pivot table with totals to a list - Step 3: Remove unnessary rows from
-    # x_tmp_pivot_list[0] is not needed. This is the sum-sum row
-    # x_tmp_pivot_list[1] Is the header for the table
-    # x_tmp_pivot_list[2] is not needed. Second header type row
-    del x_tmp_pivot_list[0:3:2]
+    log.info('Transform pivot data: Step 3 - Create table header. Transform table data')
     # last 2 rows are not needed. This is a total row, and a blank row.
     del x_tmp_pivot_list[len(x_tmp_pivot_list)-2:len(x_tmp_pivot_list)]
 
-    # Create the header row
-    headings = x_tmp_pivot_list[0]
-    log.info(f"Headers:\n {headings}")
-    del x_tmp_pivot_list[0]
+    # x_tmp_pivot_list[0] is not needed. This is the sum-sum row
+    # x_tmp_pivot_list[1] Is the header for the table
+    # x_tmp_pivot_list[2] is not needed. Second header type row
+    tbl_data = []
+    r_numb = 0
+    for x in x_tmp_pivot_list:
+        # r_numb 0, 2 are useless. x[1] is the 'weapon_id' (if 'skip' then skip)
+        if r_numb == 0 or r_numb == 2 or x[1] == 'skip': # do not append
+            log.debug(f"{r_numb} : Skipped : {x}")
+        elif r_numb == 1: # here is the header
+            log.debug(f"{r_numb} : Header : {x}")
+            headings = x
+            log.info(f"Table Header: {headings}")
+        else: # this is data. Make it pretty
+            log.debug(f"{r_numb} : MakePrtty : {x}")
+            # Columns to transform start at 2
+            for col in range(2,len(x)):
+                orgVal = x[col]
+                testVal = int(x[col])
+                if testVal > 0:
+                    # thousand seperator
+                    x[col] = '{:,}'.format(testVal)
+                else:
+                    # No value for zero's
+                    x[col] = ""
 
-    # Altering data so zeros are blank string, numbers have thousands format
-    for row in x_tmp_pivot_list:
-        #Numbers start at column/element 2
-        for col in range(2,8):
-            orgVal = row[col]
-            testVal = int(row[col])
-            if testVal > 0:
-                row[col] = '{:,}'.format(int(row[col]))
-            else:
-                row[col] = ""
-
-    log.info(f"Data:\n {x_tmp_pivot_list}")
+            tbl_data.append(x)
+        r_numb +=1
+    log.info(f"Table Data: {tbl_data}")
     log.info("Render the weapon page")
-    return render_template('weapon.html', title=f"Weapon - {weapon.title}", weapon=weapon, header_row=headings, data=x_tmp_pivot_list)
+    return render_template('weapon.html', title=f"Weapon - {weapon.title}", weapon=weapon, header_row=headings, data=tbl_data)
 
 @app.route("/resource/<id>")
 def resource_detail(id):
