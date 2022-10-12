ce4f688834e1b813a313b981077787561b256e80
dbjock (john) <junkyardjojo@hotmail.com>
2022-10-11 19:13:17 -0500

    Updating where db is located

--- hz2/__init__.py
+++ hz2/__init__.py
@@ -1,8 +1,13 @@
+import os, sys
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+
+# https://www.digitalocean.com/community/tutorials/how-to-use-flask-sqlalchemy-to-interact-with-databases-in-a-flask-application
+# the basedir and use of os.path keeps the 'instance' folder from being created
+basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data/hz2.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data','hz2.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 db = SQLAlchemy(app)
 
